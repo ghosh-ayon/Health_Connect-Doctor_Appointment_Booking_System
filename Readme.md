a840641525aee42dc5072d4e9f8ca8c83e582145
Biswadeb <biswadebmukherjee941@gmail.com>
2023-10-29 13:33:13 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+Doctor-Appointment-Booking-System/
+│
+├── .git/               # Git repository directory (created when you initialize a repository)
+├── .gitignore          # Git ignore file to specify which files and directories to exclude from version control
+│
+├── app.py              # Your Flask application
+├── config.py           # Database Configuration settings
+├── secret.py           # Secret key generator needed for the Flask web app
+├── requirements.txt    # List of Python dependencies
+│
+├── static/             # Static files (CSS, JavaScript, images)
+│   ├── admin.css       # CSS styles for the admin dashboard
+│   ├── pictures/       # Images
+│       ├── logo.png    # Site logo
+│
+├── templates/          # HTML templates
+│   ├── login.html      # Patient's login Template
+│   ├── register.html   # Patient's register Template
+│   ├── patient.html    # Patient's dashboard Template
+│
+│
+├── README.md           # Project documentation
---
+++ app.py
@@ -0,0 +1,81 @@
+import sys
+
+# Disable .pyc file generation
+sys.dont_write_bytecode = True
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from config import DATABASE_CONFIG
+from secret import SECRET_KEY
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
+app.config['MYSQL_HOST'] = DATABASE_CONFIG['host']
+app.config['MYSQL_USER'] = DATABASE_CONFIG['user']
+app.config['MYSQL_PASSWORD'] = DATABASE_CONFIG['password']
+app.config['MYSQL_DB'] = DATABASE_CONFIG['database']
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account and account['password'] == password:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return redirect(url_for('dashboard'))
+        else:
+            flash('danger', 'Incorrect username or password!')
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'name' in request.form and 'phone' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        name = request.form['name']
+        phone = request.form['phone']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            flash('danger', 'Account already exists!')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('danger', 'Invalid email address!')
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('danger', 'Username must contain only characters and numbers!')
+        elif not username or not password or not email or not name or not phone:
+            flash('danger', 'Please fill out the form!')
+        else:
+            cursor.execute('INSERT INTO users (username, password, email, name, phone) VALUES (%s, %s, %s, %s, %s)',
+                           (username, password, email, name, phone))
+            mysql.connection.commit()
+            flash('You have successfully registered!', 'You have successfully registered!')
+
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('patient.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
