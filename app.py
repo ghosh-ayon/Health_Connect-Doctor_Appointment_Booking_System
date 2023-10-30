567200986ae57935988a4e2af6954fb77f0fcddd
Biswadeb <biswadebmukherjee941@gmail.com>
2023-10-30 12:27:54 +0530

    done

--- app.py
+++ app.py
@@ -73,9 +73,14 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('patient.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
